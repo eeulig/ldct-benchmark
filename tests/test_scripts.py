ce6a7f27efbfe562967db7e73bc4861286f2de78
eeulig <contact@eeulig.com>
2025-02-26 19:04:12 +0100

    add hyperparamter optimization

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -14,3 +14,8 @@ def test_entrypoint_train_is_found():
 def test_entrypoint_test_is_found():
     exit_status = os.system("ldctbench-test --help")
     assert exit_status == 0
+
+
+def test_entrypoint_hpopt_is_found():
+    exit_status = os.system("ldctbench-hpopt --help")
+    assert exit_status == 0
